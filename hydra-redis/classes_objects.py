078bae6daa833dedf54a4f19ec65f32212513729
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-06-08 00:28:15 +0530

    update hydra_graph,collection_endpoint and classes_objects

--- hydra-redis/classes_objects.py
+++ hydra-redis/classes_objects.py
@@ -1,118 +1,148 @@
-import urllib.request as ur
+import urllib.request
 import json
 from redisgraph import Node, Edge
 
 
-class Classes:
+class ClassEndpoints:
     """Contains all the classes endpoint and the objects"""
 
     def __init__(self, redis_graph):
         self.redis_graph = redis_graph
 
-    def objects_property(self, objects_node, new_list, url_node, api_doc):
-        """Makes nodes for those properties which have properties in itself."""
-        print("for the properties which have class and should have node")
-        print(new_list)
-        for obj in new_list:
-            obj_method = []
-            obj_prop = []
-            properties = []
-            obj = obj.replace("vocab:", "")
-            objects_properties = {}
-            for obj1 in api_doc.parsed_classes[
-                                               obj][
-                                               "class"].supportedOperation:
-                obj_method.append(obj1.method)
-            print("classes", obj_method)
-            objects_properties["methods"] = str(obj_method)
-            # all the operations for the object is stored in method
-            for obj1 in api_doc.parsed_classes[obj]["class"].supportedProperty:
-                properties.append(obj1.title)
-                if obj1.prop.replace("vocab:", "") in api_doc.parsed_classes:
-                    obj_prop.append(obj1.prop.replace("vocab:", ""))
-            objects_properties["property"] = str(properties)
-            # all property which itself is an object
-            obj_properties_classlist = obj_prop
-            objects_properties["class_property"] = str(
-                obj_properties_classlist)
-            print(objects_node.alias + str(obj))
-            obj_alias = str(objects_node.alias + str(obj)).lower()
-            # key for the node of the object
-            print(obj_alias)
-            obj_node = Node(
-                label="object",
-                alias=obj_alias,
-                properties=objects_properties)
-            self.redis_graph.add_node(obj_node)
-            edge = Edge(objects_node, "has" + str(obj), obj_node)
-            self.redis_graph.add_edge(edge)
-            # set edge between the object and its parent object
-#            print("commit object property")
-#            self.redis_graph.commit()
-            # create the graph in redis
-            if obj_properties_classlist:
-                self.objects_property(
-                    obj_node, obj_properties_classlist, url_node, api_doc)
+    def addNode(self, label1, alias1, properties1):
+        """Add node to the redis graph"""
+        node = Node(label=label1, alias=alias1, properties=properties1)
+        self.redis_graph.add_node(node)
+        return node
 
-    def endpointclasses(self, classes_endpoint, url_node, api_doc, base_url):
+    def addEdge(self, source_node, predicate, dest_node):
+        """Add edge between nodes in redis graph"""
+        edge = Edge(source_node, predicate, dest_node)
+        self.redis_graph.add_edge(edge)
+
+    def get_operation(self, api_doc, endpoint):
+        """Return all the supportedOperations for given endpoint"""
+        endpoint_method = []
+
+        for support_operation in api_doc.parsed_classes[
+                endpoint][
+                "class"].supportedOperation:
+            endpoint_method.append(support_operation.method)
+        print("supportedOperation", endpoint_method)
+        # all the operations for the object is stored in endpoint_method
+
+        return str(endpoint_method)
+
+    def objects_property(
+            self,
+            objects_node,
+            new_list,
+            entrypoint_node,
+            api_doc):
+        """Nodes for every that property which is itself an object"""
+        print("for the property which is an object, should be a node")
+        for obj in new_list:
+            obj = obj.replace("vocab:", "")
+            node_properties = {}
+            properties_title = []
+            endpoint_prop = []
+            # node_properties is used for set the properties of node.
+            node_properties["operations"] = self.get_operation(api_doc, obj)
+            for support_property in api_doc.parsed_classes[
+                    obj][
+                    "class"].supportedProperty:
+                properties_title.append(support_property.title)
+                if support_property.title in api_doc.parsed_classes:
+                    endpoint_prop.append(support_property.title)
+            # store all operation and property of object
+            node_properties["property"] = str(properties_title)
+            node_properties["class_property"] = str(
+                endpoint_prop)
+            node_alias = str(objects_node.alias + str(obj)).lower()
+            # key for the node of the object
+            object_node = self.addNode("object", node_alias, node_properties)
+            print("node", object_node)
+            self.addEdge(objects_node, "has" + str(obj), object_node)
+            # set edge between the object and its parent object
+            if endpoint_prop:
+                self.objects_property(
+                    object_node, endpoint_prop, entrypoint_node, api_doc)
+
+    def endpointclasses(
+            self,
+            class_endpoints,
+            entrypoint_node,
+            api_doc,
+            base_url):
         """Node for every class which have an endpoint."""
         print("classes endpoint or accessing classes")
-
-        for obj in classes_endpoint:
-            obj_method = []
-            obj_prop = []
-            properties = []
-            obj_properties = {}
+        endpoint_property_list = {}
+        # contain all endpoints which have other endpoints as a property.
+        for endpoint in class_endpoints:
+            node_properties = {}
+            # node_properties is used for set the properties of node.
             member = {}
+            # member contain the properties of class endpoint.
+            endpoint_property = []
+            # it contains the property which is a non-endpoint object or class.
+            property_list = []
             print("check endpoint url....loading data")
-            new_url = base_url + classes_endpoint[obj]
+            new_url = base_url + \
+                class_endpoints[endpoint].replace("vocab:EntryPoint", "")
             # url for the classes endpoint
             print(new_url)
-            response = ur.urlopen(new_url)
+            response = urllib.request.urlopen(new_url)
             new_file = json.loads(response.read().decode('utf-8'))
             # retreiving data for the classes endpoint from server
-            if obj in api_doc.parsed_classes:
-                for obj1 in api_doc.parsed_classes[obj]["class"
-                                                        ].supportedOperation:
-                    obj_method.append(obj1.method)
-                print("classes ", obj_method)
-                obj_properties["methods"] = str(obj_method)
-                # store the operations for the endpoint
-                for obj1 in api_doc.parsed_classes[obj]["class"
-                                                        ].supportedProperty:
-                    properties.append(obj1.title)
-                    if obj1.prop.replace(
-                            "vocab:", "") in api_doc.parsed_classes:
-                        obj_prop.append(obj1.prop.replace("vocab:", ""))
-                obj_properties["property"] = str(properties)
-                obj_properties_classlist = obj_prop
-                obj_properties["class_property"] = str(
-                    obj_properties_classlist)
-                # store all those properties which is itself an object
-                for obj1 in api_doc.parsed_classes[obj]["class"
-                                                        ].supportedProperty:
-                    if isinstance(new_file[obj1.title], str):
-                        member[obj1.title] = str(
-                            new_file[obj1.title].replace(" ", ""))
+            node_properties["operations"] = self.get_operation(
+                api_doc, endpoint)
+            # store the operations for the endpoint
 
+            for support_property in api_doc.parsed_classes[
+                                                   endpoint][
+                                                   "class"].supportedProperty:
+                if endpoint != support_property.title:
+                    if support_property.title in class_endpoints:
+                        property_list.append(support_property.title)
+                    elif support_property.title in api_doc.parsed_classes:
+                        endpoint_property.append(support_property.title)
+
+                if isinstance(new_file[support_property.title], str):
+                    if support_property.title in new_file:
+                        member[support_property.title] = str(
+                            new_file[support_property.title].replace(" ", ""))
+                    else:
+                        member[support_property.title] = "null"
+            supported_properties_list = endpoint_property
+            endpoint_property_list[endpoint] = property_list
+            node_properties["class_property"] = str(
+                supported_properties_list)
+            # member is using for storing the fetched data in node.
             member["@id"] = str(new_file["@id"])
             member["@type"] = str(new_file["@type"])
-            obj_properties["property"] = str(member)
-            class_object_node = Node(
-                label="classes",
-                alias=str(obj),
-                properties=obj_properties)
-            self.redis_graph.add_node(class_object_node)
-            edge = Edge(url_node, "has" + obj, class_object_node)
-            self.redis_graph.add_edge(edge)
+            node_properties["property"] = str(member)
+            class_object_node = self.addNode(
+                "classes", str(endpoint), node_properties)
+            self.addEdge(entrypoint_node, "has" + endpoint, class_object_node)
             # set edge between the entrypoint and the class endpoint/object
-#            print("commit classesendpoint")
-#            self.redis_graph.commit()
-            # create the graph in redis
-            if obj_properties_classlist:
-                print("in obj_properties_classlist", obj_properties_classlist)
+            if supported_properties_list:
                 self.objects_property(
                     class_object_node,
-                    obj_properties_classlist,
-                    url_node,
+                    supported_properties_list,
+                    entrypoint_node,
                     api_doc)
+        # for connect the nodes to endpoint which have endpoint as a property.
+        if endpoint_property_list:
+            for endpoint_property in endpoint_property_list:
+                for src_node in self.redis_graph.nodes.values():
+                    if str(endpoint_property) == src_node.alias:
+                        for endpoints in endpoint_property_list[
+                                               endpoint_property]:
+                            for nodes in self.redis_graph.nodes.values():
+                                if endpoints == nodes.alias:
+                                    self.addEdge(
+                                        src_node,
+                                        "has_endpoint_property",
+                                        nodes)
+                                    break
+                        break
--- hydra-redis/collections_endpoint.py
+++ hydra-redis/collections_endpoint.py
@@ -1,108 +1,109 @@
-import urllib.request as ur
+import urllib.request
 import json
 import re
-from redisgraph import Node, Edge
-from classes_objects import Classes
+from classes_objects import ClassEndpoints
 
 
-class Collections:
+class CollectionEndpoints:
     """Contains all the collections endpoints and objects"""
 
-    def __init__(self, redis_graph):
+    def __init__(self, redis_graph, class_endpoints):
         self.redis_graph = redis_graph
+        self.class_endpoints = class_endpoints
+
+    def fetch_data(self, new_url):
+        """Fetching data from the server"""
+        response = urllib.request.urlopen(new_url)
+        return json.loads(response.read().decode('utf-8'))
 
     def collectionobjects(
             self,
-            obj_collection_node,
-            new_file,
+            endpoint_collection_node,
+            endpoint_list,
             new_url,
-            url_node,
+            entrypoint_node,
             api_doc,
-            base_url):
+            url):
         """Creating nodes for all objects stored in collection."""
-        objects = new_file
-        if isinstance(objects, str):
-            objects = objects.replace("[", "")
-            objects = objects.replace("]", "")
-
-        obj_properties = {}
         print("accesing the collection object like events or drones")
-        if objects:
-            for obj in objects:
-                obj_prop = []
-                obj_method = []
+        if endpoint_list:
+            clas = ClassEndpoints(self.redis_graph)
+            for endpoint in endpoint_list:
+                node_properties = {}
+                no_endpoint_property = []
+                endpoint_method = []
                 member = {}
+                endpoint_property_list = []
                 match_obj = re.match(
-                    r'/(.*)/(.*)/(.*)?', obj["@id"], re.M | re.I)
+                    r'/(.*)/(.*)/(.*)?', endpoint["@id"], re.M | re.I)
                 base_url = "/{0}/{1}/".format(match_obj.group(1),
                                               match_obj.group(2))
-                entrypoint_member = obj["@type"].lower() + match_obj.group(3)
+                entrypoint_member = endpoint["@type"].lower(
+                ) + match_obj.group(3)
                 print(base_url, entrypoint_member)
                 member_alias = entrypoint_member
                 # key for the object node is memeber_alias
                 member_id = match_obj.group(3)
-                print("event alias and id", member_alias, member_id)
+                print("member alias and id", member_alias, member_id)
                 new_url1 = new_url + "/" + member_id
-                response = ur.urlopen(new_url1)
-                new_file1 = json.loads(response.read().decode('utf-8'))
+                new_file1 = self.fetch_data(new_url1)
                 # object data retrieving from the server
-                if obj["@type"] in api_doc.parsed_classes:
-                    for obj2 in api_doc.parsed_classes[obj["@type"]
-                                                       ]["class"
-                                                         ].supportedOperation:
-                        obj_method.append(obj2.method)
-                    obj_properties["methods"] = str(obj_method)
-                    # all the operations for the object is stored in method
-                    for obj2 in api_doc.parsed_classes[obj["@type"]
-                                                       ]["class"
-                                                         ].supportedProperty:
-                        if obj2.prop.replace(
-                                "vocab:", "") in api_doc.parsed_classes:
-                            obj_prop.append(obj2.prop.replace("vocab:", ""))
-                    obj_properties_classlist = obj_prop
-                    obj_properties["class_property"] = str(
-                        obj_properties_classlist)
-                    # all property which itself is an object
-                    for obj1 in api_doc.parsed_classes[obj["@type"]
-                                                       ]["class"
-                                                         ].supportedProperty:
-                        if isinstance(new_file1[obj1.title], str):
-                            member[obj1.title] = str(
-                                new_file1[obj1.title].replace(" ", ""))
+                for support_operation in api_doc.parsed_classes[
+                                                      endpoint["@type"]
+                                                      ]["class"
+                                                        ].supportedOperation:
+                    endpoint_method.append(support_operation.method)
+                node_properties["methods"] = str(endpoint_method)
+                # all the operations for the object is stored in method
+                for support_property in api_doc.parsed_classes[
+                                                      endpoint["@type"]
+                                                      ]["class"
+                                                        ].supportedProperty:
+                    if support_property.title in self.class_endpoints:
+                        endpoint_property_list.append(
+                            str(support_property.title))
+                    elif support_property.title in api_doc.parsed_classes:
+                        no_endpoint_property.append(support_property.title)
 
-                member["@id"] = str(obj["@id"])
-                member["@type"] = str(obj["@type"])
-                obj_properties["property"] = str(member)
-                objects_node = Node(
-                    label="objects",
-                    alias=str(member_alias),
-                    properties=obj_properties)
-                self.redis_graph.add_node(objects_node)
+                    if isinstance(new_file1[support_property.title], str):
+                        if support_property.title in new_file1:
+                            member[support_property.title] = str(
+                                new_file1[
+                                    support_property.title].replace(" ", ""))
+                        else:
+                            member[support_property.title] = "null"
+                no_endpoint_list = no_endpoint_property
+                node_properties["class_property"] = str(
+                    no_endpoint_list)
+                # all property which itself is an object
+
+                member["@id"] = str(endpoint["@id"])
+                member["@type"] = str(endpoint["@type"])
+                node_properties["property"] = str(member)
+                collection_object_node = clas.addNode(
+                    "objects", str(member_alias), node_properties)
                 # add object as a node in redis
-#                print("commiting collection object", member_alias)
-#                self.redis_graph.commit()
-#                print(
-#                    "property of obj which can be class")
-                edge = Edge(obj_collection_node, "has_" +
-                            str(obj["@type"]),
-                            objects_node)
-                self.redis_graph.add_edge(edge)
+                clas.addEdge(endpoint_collection_node, "has_" +
+                             str(endpoint["@type"]), collection_object_node)
                 # set an edge between the collection and its object
-#                print("commiting collection object", member_alias)
-#                self.redis_graph.commit()
-                # put/create the graph in redis
                 print(
                     "property of obj which can be class",
-                    obj_properties["class_property"],
+                    node_properties["class_property"],
                     "collection"
                 )
-
-                if obj_properties_classlist:
-                    c = Classes(self.redis_graph)
-                    c.objects_property(
-                        objects_node,
-                        obj_properties_classlist,
-                        url_node,
+                if endpoint_property_list:
+                    for endpoint_property in endpoint_property_list:
+                        for nodes in self.redis_graph.nodes.values():
+                            if endpoint_property == nodes.alias:
+                                clas.addEdge(
+                                    collection_object_node,
+                                    "hasendpointproperty",
+                                    nodes)
+                if no_endpoint_list:
+                    clas.objects_property(
+                        collection_object_node,
+                        no_endpoint_list,
+                        entrypoint_node,
                         api_doc)
         else:
             print("NO MEMBERS")
@@ -110,52 +111,45 @@ class Collections:
     def endpointCollection(
             self,
             collection_endpoint,
-            url_node,
+            entrypoint_node,
             api_doc,
-            base_url):
+            url):
         """It makes a node for every collection endpoint."""
         print("accessing every collection in entrypoint")
-        for obj in collection_endpoint:
-            obj_method = []
-            properties = []
-            obj_properties = {}
+        clas = ClassEndpoints(self.redis_graph)
+        for endpoint in collection_endpoint:
+            endpoint_method = []
+            node_properties = {}
             print(
                 "check url for endpoint",
-                base_url +
-                collection_endpoint[obj])
-            new_url = base_url + collection_endpoint[obj]
+                url +
+                collection_endpoint[endpoint].replace("vocab:EntryPoint", ""))
+            new_url = url + \
+                collection_endpoint[endpoint].replace("vocab:EntryPoint", "")
             # url for every collection endpoint
-            for obj2 in api_doc.collections[
-                                            obj][
-                                            "collection"].supportedOperation:
-                obj_method.append(obj2.method)
-            obj_properties["methods"] = str(obj_method)
+            for support_operation in api_doc.collections[
+                    endpoint][
+                    "collection"].supportedOperation:
+                endpoint_method.append(support_operation.method)
+            node_properties["operations"] = str(endpoint_method)
             # all the operations for the collection endpoint is stored in
-            # method
-            print(obj_properties["methods"])
-            for obj2 in api_doc.collections[
-                                            obj][
-                                            "collection"].supportedProperty:
-                properties.append(str(obj2.desc[4:]))
-            obj_properties["title"] = str(properties)
-            response = ur.urlopen(base_url + collection_endpoint[obj])
-            new_file = json.loads(response.read().decode('utf-8'))
+            print(node_properties["operations"])
+            node_properties["@id"] = str(collection_endpoint[endpoint])
+            new_file = self.fetch_data(new_url)
             # retrieving the objects from the collection endpoint
-            obj_properties["members"] = str(new_file["members"])
-            obj_collection_node = Node(
-                label="collection", alias=obj, properties=obj_properties)
-            self.redis_graph.add_node(obj_collection_node)
-            edge = Edge(url_node, "has_collection", obj_collection_node)
-            self.redis_graph.add_edge(edge)
+            node_properties["members"] = str(new_file["members"])
+            endpoint_collection_node = clas.addNode(
+                "collection", endpoint, node_properties)
+            clas.addEdge(
+                entrypoint_node,
+                "has_collection",
+                endpoint_collection_node)
             # set an edge between the entrypoint and collection endpoint
-#            print("commit endpoint collection")
-#            self.redis_graph.commit()
-            # create the graph in redis
             self.collectionobjects(
-                obj_collection_node,
+                endpoint_collection_node,
                 new_file["members"],
                 new_url,
-                url_node,
+                entrypoint_node,
                 api_doc,
-                base_url
+                url
             )
