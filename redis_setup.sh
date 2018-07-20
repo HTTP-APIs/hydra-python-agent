3c1eedd0c9cfa6eafa8612146dd31a70c647d9b1
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-07-20 18:21:42 +0530

    update redis-setup.sh

--- redis_setup.sh
+++ redis_setup.sh
@@ -1,5 +1,8 @@
 
 #It will remove all the previous docker engine
+docker -v
+if [ echo "$?" = "127" ]
+then
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
 # Now install docker repo
@@ -15,10 +18,19 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 sudo apt-get update
 sudo apt-get install docker-ce
 
+else
+    echo "yesss exist"
+fi
 
 # after getting the docker-ce
-git clone https://github.com/swilly22/redis-graph.git
-cd redis-graph
-sudo make docker
+if [ -z "$(docker images -q redislabs/redisgraph:latest)" ]
+then
+    echo "yes exist"
+
+else 
+    git clone https://github.com/swilly22/redis-graph.git
+    cd redis-graph
+    sudo make docker
+fi
 # command for run the server 
 sudo docker run -p 6379:6379 redislabs/redisgraph
