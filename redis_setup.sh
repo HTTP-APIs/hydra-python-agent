fccbb496b098c6c04944e88280bdf48d55c2b975
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-08-05 19:57:59 +0530

    update redis_setup.sh

--- redis_setup.sh
+++ redis_setup.sh
@@ -3,20 +3,9 @@
 docker -v
 if [ echo "$?" = "127" ]
 then
-sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
-# Now install docker repo
-sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
- 2023  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-  
-# install docker-ce 
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker
+sudo apt-get install docker-compose
 
 else
     echo "Docker is already installed"
@@ -28,9 +17,7 @@ then
     echo "Docker already have a redislabs/redisgraph image"
 
 else 
-    git clone https://github.com/swilly22/redis-graph.git
-    cd redis-graph
-    sudo make docker
+    sudo docker run -p 6379:6379 -it --rm redislabs/redisgraph
 fi
 # command for run the server 
-#sudo docker run -p 6379:6379 redislabs/redisgraph # uncomment this line if you want to run server without using dockerflie.
+# sudo docker run -p 6379:6379 -it --rm redislabs/redisgraph # uncomment this line if you want to run server without using dockerflie.
