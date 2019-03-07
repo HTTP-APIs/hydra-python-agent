36dc31ccad12a2cfc44757193e7ead0d4196594a
ShravanDoda <shravanbharatdoda@gmail.com>
2019-03-07 18:19:06 +0530

    Updated tests for to comply with use of context-manager in urllib requests

--- hydra_agent/tests/test_querying.py
+++ hydra_agent/tests/test_querying.py
@@ -5,18 +5,30 @@ from hydra_agent.querying_mechanism import EndpointQuery, CollectionmembersQuery
 
 
 class TestHandleData(unittest.TestCase):
+    """
+    TestCase for HandleData Class
+    """
     def setUp(self):
+        """Setting up HandleData object"""
         self.handle_data = HandleData()
 
     @patch('hydra_agent.querying_mechanism.json.loads', spec_set=True)
     @patch('hydra_agent.querying_mechanism.urllib.request.urlopen', spec_set=True)
     def test_load_data(self, request_mock, json_mock):
+        """
+        Tests load_data method case without exceptions
+
+        Args:
+            request_mock : MagicMock object from patching out urllib.request.urlopen function
+            json_mock: MagicMock object from patching out json.loads function
+        """
+
         # url passed to load_data as an argument
         url = "TestURL"
 
         # mock to return byte code for response.read()
         intermediate_mock = MagicMock()
-        intermediate_mock.read.return_value = b"TestResponse"
+        intermediate_mock.__enter__.return_value.read.return_value = b"TestResponse"
 
         # making sure that request doesn't raise an exception
         request_mock.return_value = intermediate_mock
@@ -31,6 +43,14 @@ class TestHandleData(unittest.TestCase):
     @patch('hydra_agent.querying_mechanism.logger')
     @patch('hydra_agent.querying_mechanism.urllib.request.urlopen', spec_set=True)
     def test_load_data_with_errors(self, request_mock, logger_mock, json_mock):
+        """
+        Tests load_data method case with exception
+
+        Args:
+            request_mock : MagicMock object from patching out urllib.request.urlopen function
+            logger_mock : MagicMock object from patching out the logger object
+            json_mock : MagicMock object from patching out the json.loads function
+        """
         # url passed to load_data as an argument
         url = "TestURL"
         # request raises an exception (ValueError can be replaced by URLError or HTTPError)
@@ -44,11 +64,23 @@ class TestHandleData(unittest.TestCase):
 
 
 class TestEndpointQuery(unittest.TestCase):
+    """
+    TestCase for EndpointQuery Class
+    """
+
     @patch('hydra_agent.querying_mechanism.RedisProxy', autospec=True)
     def setUp(self, redis_mock):
+        """
+        Setting up EndpointQuery object
+        Args:
+            redis_mock : MagicMock object from patching out RedisProxy
+        """
         self.endpoint_query = EndpointQuery()
 
     def test_get_allEndpoints(self):
+        """
+        Tests get_allEndpoints method
+        """
         # query to be passed as a param to get_allEndpoints
         query = "get endpoints"
 
@@ -64,6 +96,9 @@ class TestEndpointQuery(unittest.TestCase):
         connection_mock.execute_command.assert_has_calls(calls)
 
     def test_get_classEndpoints(self):
+        """
+        Tests get_classEndpoints method
+        """
         # query to be passed as a param to get_classEndpoints
         query = "get classEndpoints"
 
@@ -78,6 +113,9 @@ class TestEndpointQuery(unittest.TestCase):
         connection_mock.execute_command.assert_has_calls(calls)
 
     def test_get_collectionEndpoints(self):
+        """
+        Tests get_collectionEndpoints method
+        """
         # query to be passed as a param to get_collectionEndpoints
         query = "get collectionEndpoints"
 
@@ -94,15 +132,29 @@ class TestEndpointQuery(unittest.TestCase):
 
 
 class TestCollectionmembersQuery(unittest.TestCase):
+    """
+    TestCase for CollectionmembersQuery class
+    """
     @patch('hydra_agent.querying_mechanism.CollectionEndpoints', autospec=True)
     @patch('hydra_agent.querying_mechanism.RedisProxy', autospec=True)
     def setUp(self, redis_mock, collections_mock):
+        """
+        Setting up CollectionmembersQuery object
+
+        Args:
+            redis_mock : MagicMock object from patching out RedisProxy
+            collections_mock: MagicMock object from patching out CollectionEndpoints
+        """
+
         api_doc = MagicMock()
         url = MagicMock()
         graph = MagicMock()
         self.cmq = CollectionmembersQuery(api_doc, url, graph)
 
     def test_data_from_server(self):
+        """
+        Tests data_from_server method
+        """
         # endpoint param to be passed to data_from_server
         endpoint = "TestEndpoint"
 
@@ -123,11 +175,20 @@ class TestCollectionmembersQuery(unittest.TestCase):
         connection_mock.execute_command.assert_has_calls(calls)
 
     def smembers_mock_func(self, inp):
+        """
+        SideEffect for testing get_members
+
+        Args:
+            inp : Input to mock object's side effect equivalent to smembers method
+        """
         if inp == "fs:endpoints":
             return [b"TestEndpoint"]
 
     def test_get_members_if(self):
         # query param to be passed to get_members
+        """
+        Tests get_members method when if condition passes
+        """
         query = "TestEndpoint members"
         connection_mock = self.cmq.connection
 
@@ -147,10 +208,19 @@ class TestCollectionmembersQuery(unittest.TestCase):
         connection_mock.sadd.assert_not_called()
 
     def smembers_mock_func_else(self, inp):
+        """
+        SideEffect for mock object used in testing get_members_method
+
+        Args:
+            inp: Input to Mock object's side effect equivalent to smembers method
+        """
         if inp == "fs:endpoints":
             return [b"TestEndpoint"]
 
     def test_get_members_else(self):
+        """
+        Tests get_members method when if condition fails
+        """
         # query param to be passed to get_members
         query = "TestEndpoint members"
         connection_mock = self.cmq.connection
@@ -167,11 +237,23 @@ class TestCollectionmembersQuery(unittest.TestCase):
 
 
 class TestPropertiesQuery(unittest.TestCase):
+    """
+    TestCase for PropertiesQuery Class
+    """
     @patch('hydra_agent.querying_mechanism.RedisProxy')
     def setUp(self, redis_mock):
+        """
+        Setting up PropertiesQuery object
+
+        Args:
+            redis_mock : MagicMock object from patching out RedisProxy
+        """
         self.properties_query = PropertiesQuery()
 
     def test_get_classes_properties(self):
+        """
+        Tests get_classes_properties method
+        """
         # query param for get_classes_properties
         query = "classClassEndpoint properties"
         connection_mock = self.properties_query.connection
@@ -183,6 +265,9 @@ class TestPropertiesQuery(unittest.TestCase):
         connection_mock.execute_command.assert_called_with('GRAPH.QUERY', 'apidoc', 'MATCH ( p:classes ) WHERE (p.type="ClassEndpoint") RETURN p.properties')
 
     def test_get_collection_properties(self):
+        """
+        Tests get_collection_properties method
+        """
         # query param for get_collection_properties
         query = "collectionEndpoint properties"
         connection_mock = self.properties_query.connection
@@ -194,6 +279,9 @@ class TestPropertiesQuery(unittest.TestCase):
         connection_mock.execute_command.assert_called_with('GRAPH.QUERY', 'apidoc', 'MATCH ( p:collection ) WHERE (p.type="collectionEndpoint") RETURN p.properties')
 
     def test_members_properties(self):
+        """
+        Tests get_members_properties method
+        """
         # query param for get_members_properties
         query = "testMember properties"
 
@@ -206,6 +294,9 @@ class TestPropertiesQuery(unittest.TestCase):
         connection_mock.execute_command.assert_called_with('GRAPH.QUERY', 'apidoc', 'MATCH ( p:testMember ) RETURN p.id,p.properties')
 
     def test_object_properties(self):
+        """
+        Tests get_object_property method
+        """
         #  query for get_object_property
         query = "object/api/TestCollection/2 properties"
 
@@ -219,15 +310,28 @@ class TestPropertiesQuery(unittest.TestCase):
 
 
 class TestClassPropertiesValue(unittest.TestCase):
+    """
+    TestCase for ClassPropertiesValue
+    """
     @patch('hydra_agent.querying_mechanism.ClassEndpoints', autospec=True)
     @patch('hydra_agent.querying_mechanism.RedisProxy')
     def setUp(self, redis_mock, classEndpoint_mock):
+        """
+        Setting up ClassPropertiesValue object
+
+        Args:
+            redis_mock : MagicMock object from patching out RedisProxy
+            classEndpoint_mock : MagicMock object from patching out ClassEndpoint class
+        """
         api_doc = MagicMock()
         url = MagicMock()
         graph = MagicMock()
         self.cpv = ClassPropertiesValue(api_doc, url, graph)
 
     def test_data_from_server(self):
+        """
+        Tests data_from_server method
+        """
         # endpoint param for data_from_server
         endpoint = "TestEndpoint"
         clas_mock = self.cpv.clas
@@ -245,10 +349,19 @@ class TestClassPropertiesValue(unittest.TestCase):
                RETURN p.property_value""")
 
     def smembers_mock_func(self, inp):
+        """
+        SideEffect for mock object used in testing get_property_value
+
+        Args:
+            inp : Input to the SideEffect equivalent to smembers method
+        """
         if inp == "fs:endpoints":
             return [b"TestClass"]
 
     def test_get_property_value(self):
+        """
+        Tests get_property_value when if condition passes
+        """
         # query param for get_property_value
         query = "classTestClass property_value"
         connection_mock = self.cpv.connection
@@ -269,9 +382,15 @@ class TestClassPropertiesValue(unittest.TestCase):
         connection_mock.sadd.assert_not_called()
 
     def smembers_mock_func_else(self, inp):
+        """
+        SideEffect for mock object used in testing get_property_value
+        """
         return []
 
     def test_get_property_value_else(self):
+        """
+        Tests get_property_value method when the if condition fails
+        """
         # query param for get_property_value
         query = "classTestClass property_value"
         connection_mock = self.cpv.connection
