383a447e01e48d57c315a3f824c71c20beb40028
Gustavo Morais <gustavopguto@gmail.com>
2019-06-13 17:36:15 +0200

    Added POST and PUT
    Also status checking now

--- hydra_agent/agent.py
+++ hydra_agent/agent.py
@@ -1,6 +1,5 @@
-from hydra_agent.requests import Requests
-from hydra_agent.redis_proxy import RedisProxy
-from hydra_agent.sync_mechanism import SynchronizationProcessing
+from redis_proxy import RedisProxy
+from sync_mechanism import SynchronizationProcessing
 from requests import Session
 
 
@@ -14,19 +13,47 @@ class HydraAgent(Session):
 
     def get(self, url):
         response = super().get(url)
-        self.sync_processing.add_operation("GET", url)
+
+        if response.status_code == 200:
+            self.sync_processing.add_operation("GET", url)
+
+        return response
+
+    def put(self, url, new_object):
+        response = super().put(url, json=new_object)
+
+        if response.status_code == 201:
+            url = response.headers['Location']
+            self.sync_processing.add_operation("PUT", url)
+
+        return response
+
+    def post(self, url, updated_object):
+        response = super().post(url, json=updated_object)
+
+        if response.status_code == 200:
+            self.sync_processing.add_operation("POST", url)
 
         return response
 
     def delete(self, url):
         response = super().delete(url)
-        self.sync_processing.add_operation("DELETE", url)
+
+        if response.status_code == 200:
+            self.sync_processing.add_operation("DELETE", url)
 
         return response
 
 if __name__ == "__main__":
     HydraAgent = HydraAgent("http://localhost:8080/serverapi")
 
-    # print(HydraAgent.get("http://localhost:8080/serverapi/DroneCollection/"))
-    print(HydraAgent.delete("http://localhost:8080/serverapi/DroneCollection/255e9a19-010d-4dab-ad9c-329316c20246"))
-    
+    new_object = {"@type": "Drone", "DroneState": "Simplified state",
+                  "name": "Smart Drone", "model": "Hydra Drone",
+                  "MaxSpeed": "999", "Sensor": "Wind"}
+
+    print(HydraAgent.get("http://localhost:8080/serverapi/DroneCollection/"))
+    # print(HydraAgent.put("http://localhost:8080/serverapi/DroneCollection/",
+    #      new_object))
+    # print(HydraAgent.post("http://localhost:8080/serverapi/DroneCollection/ec86b237-d75d-4911-9220-8e02ea4ef860",
+    #      new_object))
+    # print(HydraAgent.delete("http://localhost:8080/serverapi/DroneCollection/255e9a19-010d-4dab-ad9c-329316c20246"))
