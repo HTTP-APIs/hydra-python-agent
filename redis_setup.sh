1d5f657634371ba3699bd8bd76628965f0d82aa1
Gustavo Morais <gustavopguto@gmail.com>
2019-06-27 16:59:12 +0200

    Setting Docker Image version for Redis Graph

--- redis_setup.sh
+++ redis_setup.sh
@@ -12,12 +12,12 @@ else
 fi
 
 # after getting the docker-ce, check if `redislabs/redisgraph` docker image is not installed then install ii. 
-if [ -z "$(docker images -q redislabs/redisgraph:latest)" ]
+if [ -z "$(docker images -q redislabs/redisgraph:1.2.2)" ]
 then
-    echo "Docker already have a redislabs/redisgraph image"
+    echo "Docker already have a redislabs/redisgraph:1.2.2 image"
 
 else 
-    sudo docker run -p 6379:6379 -it --rm redislabs/redisgraph
+    sudo docker run -p 6379:6379 -it --rm redislabs/redisgraph:1.2.2
 fi
 # command for run the server 
 # sudo docker run -p 6379:6379 -it --rm redislabs/redisgraph # uncomment this line if you want to run server without using dockerflie.
