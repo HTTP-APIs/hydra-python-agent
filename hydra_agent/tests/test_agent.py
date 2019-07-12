4cc3e449761009d5dd2c7dac5955dde2b587dc79
Gustavo Morais <gustavopguto@gmail.com>
2019-07-12 16:07:22 -0300

    Adding Test Edges/Relationships

--- hydra_agent/tests/test_agent.py
+++ hydra_agent/tests/test_agent.py
@@ -2,6 +2,8 @@ import unittest
 from unittest.mock import patch, MagicMock, call
 from hydra_agent.agent import Agent
 from hydra_agent.redis_core.graphutils_operations import GraphOperations
+from hydra_agent.redis_core.redis_proxy import RedisProxy
+from redisgraph import Graph
 from hydra_agent.tests.test_examples.hydra_doc_sample import doc as drone_doc
 
 
@@ -20,10 +22,13 @@ class TestAgent(unittest.TestCase):
         get_session_mock.return_value.json.return_value = drone_doc
 
         self.agent = Agent("http://localhost:8080/api")
+        self.redis_proxy = RedisProxy()
+        self.redis_connection = self.redis_proxy.get_connection()
+        self.redis_graph = Graph("apigraph", self.redis_connection)
 
     @patch('hydra_agent.agent.Session.get')
     def test_get_url(self, get_session_mock):
-        """Tests get method from the Agent
+        """Tests get method from the Agent 
         :param get_session_mock: MagicMock object for patching session.get
         """
         state_object = {"@id": "/api/StateCollection/1", "@type": "State",
@@ -231,6 +236,52 @@ class TestAgent(unittest.TestCase):
 
         # Assert if nothing different was returned by Redis
         self.assertEqual(get_new_object, {"msg": "resource doesn't exist"})
+
+    @patch('hydra_agent.agent.Session.get')
+    @patch('hydra_agent.agent.Session.put')
+    def test_edges(self, put_session_mock, embedded_get_mock):
+        """Tests put method from the Agent
+        :param put_session_mock: MagicMock object for patching session.put
+        """
+        new_object = {"@type": "Drone", "DroneState": "/api/StateCollection/1",
+                      "name": "Smart Drone", "model": "Hydra Drone",
+                      "MaxSpeed": "999", "Sensor": "Wind"}
+
+        collection_url = "http://localhost:8080/api/DroneCollection/"
+        new_object_url = collection_url + "1"
+
+        put_session_mock.return_value.status_code = 201
+        put_session_mock.return_value.json.return_value = new_object
+        put_session_mock.return_value.headers = {'Location': new_object_url}
+
+        state_object = {"@context": "/api/contexts/StateCollection.jsonld",
+                        "@id": "/api/StateCollection/1", "@type": "State",
+                        "Battery": "sensor Ex", "Direction": "speed Ex",
+                        "DroneID": "sensor Ex", "Position": "model Ex",
+                        "SensorStatus": "sensor Ex", "Speed": "2"}
+
+        # Mocking an object to be used for a property that has an embedded link
+        embedded_get_mock.return_value.status_code = 200
+        embedded_get_mock.return_value.json.return_value = state_object
+
+        response, new_object_url = self.agent.put(collection_url, new_object)
+
+        # Checking if Drone Collection has an edge to the Drone Resource
+        query = "MATCH (p)-[r]->() WHERE p.type = 'DroneCollection' \
+            RETURN type(r)"
+        query_result = self.redis_graph.query(query)
+        self.assertEqual(query_result.result_set[0][0], 'has_Drone')
+
+        # Checking if State Collection has an edge to the State Resource
+        query = "MATCH (p)-[r]->() WHERE p.type = 'StateCollection' \
+            RETURN type(r)"
+        query_result = self.redis_graph.query(query)
+        self.assertEqual(query_result.result_set[0][0], 'has_State')
+
+        # Checking if Drone Resource has an edge to the State Resource
+        query = "MATCH (p)-[r]->() WHERE p.type = 'Drone' RETURN type(r)"
+        query_result = self.redis_graph.query(query)
+        self.assertEqual(query_result.result_set[0][0], 'has_State')
 
 if __name__ == "__main__":
     unittest.main()
