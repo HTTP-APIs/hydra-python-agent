9c282cabd7237d28b92871b739fccddc02c22e88
Gustavo Morais <gustavopguto@gmail.com>
2019-08-05 18:19:16 -0300

    Adding GRID View with Graph and Console components

--- gui_agent/console-frontend/src/app/app.js
+++ gui_agent/console-frontend/src/app/app.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
 import NavBar from '../components/navbar/NavBar'
+import HydraConsole from '../components/hydra-console/HydraConsole'
+import HydraGraph from '../components/hydra-graph/HydraGraph'
+import Grid from '@material-ui/core/Grid';
 import './app.scss';
 
 
 export default function App() {
   return (
     <div>
-      <NavBar text="rs" backgroundColor='primary' color='red'></NavBar>
-      <header className="app-header">
-        
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/app/app.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar text="rs" backgroundColor='primary' color='primary'></NavBar>
+      <Grid container>
+      <Grid item xs={6}> 
+          <HydraGraph></HydraGraph>
+      </Grid>
+
+      <Grid item xs={6}> 
+        <HydraConsole></HydraConsole>
+      </Grid>
+
+      </Grid>
     </div>
   );
 }
