f4efe0f4ca64b1822221d9f5345226e045cdd0be
Gustavo Morais <gustavopguto@gmail.com>
2019-08-06 18:23:50 -0300

    Add toolbar and style different buttons

--- gui_agent/console-frontend/src/components/hydra-console/HydraConsole.jsx
+++ gui_agent/console-frontend/src/components/hydra-console/HydraConsole.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 class HydraConsole extends React.Component {
+    constructor(props) {
+        super(props);
+        this.classes = {
+            button: {
+              margin: '2px',
+            },
+            input: {
+              display: 'none',
+            },
+            outContainer: {
+                height: '40vh',
+                backgroundColor: '#000',
+            }
+        }
+    }
+
    render() {
-       return <h1> The interactive dynamic console will be here</h1>
+       
+        return (
+            <Grid container className={this.classes.outContainer} style={this.classes.outContainer}>
+                <Grid item xs={4} container
+                    direction="column"
+                    justify="space-between"
+                    alignItems="center"> 
+                    <Button variant="contained" color="secondary" className={this.classes.button} >
+                        Drone Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                    
+                </Grid>
+                <Grid item xs={4}> 
+                    <Button variant="contained" color="secondary" className={this.classes.button} >
+                        Drone Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                </Grid>
+                <Grid item xs={4}> 
+                    <Button variant="contained" color="secondary" className={this.classes.button} >
+                        Drone Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                    <Button variant="contained" className={this.classes.button}>
+                        State Collection
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="secondary" href="#contained-buttons" className={this.classes.button}>
+                        Send Request
+                    </Button>
+                </Grid>   
+            </Grid>
+        )
     }
 }
 
