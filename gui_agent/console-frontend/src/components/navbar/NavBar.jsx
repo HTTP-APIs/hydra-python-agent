942ee074a2e193527743787fd1a9389f31284d69
Gustavo Morais <gustavopguto@gmail.com>
2019-08-07 16:40:20 -0300

    Adding Hydra Eco's logo link to toolbar

--- gui_agent/console-frontend/src/components/navbar/NavBar.jsx
+++ gui_agent/console-frontend/src/components/navbar/NavBar.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import logo from '../../assets/images/hydra_eco_logo.png';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    hydraEcoLogo: {
+      maxWidth: '30px',
+      cursor: 'pointer'
+    },
+  }));
 
 const NavBar = (props) => {
+    const classes = useStyles();
+
     const toolbar = {
         AppBar: {
             backgroundColor: props.backgroundColor,
         },
         Typography: {
             fontSize: props.fontSize,
+            flexGrow: 1,
         }
     };
 
@@ -20,6 +32,9 @@ const NavBar = (props) => {
                     <Typography variant="title" style={toolbar.Typography} color={props.fontColor}>
                         {props.text}
                     </Typography>
+                    {props.onClick && (
+                    <img src={logo} onClick={props.onClick}  className={classes.hydraEcoLogo} alt="logo" />
+                    )}
                 </Toolbar>
             </AppBar>
         </div>
