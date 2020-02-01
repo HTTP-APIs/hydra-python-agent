b439c1e991380fbe503176a42540875cb9734452
PrathamDogra <dogra.pratham@gmail.com>
2020-02-01 15:10:29 +0530

    Removed the parsed requirements issue

--- setup.py
+++ setup.py
@@ -3,15 +3,22 @@
 
 from setuptools import setup, find_packages
 
-try:  # for pip >= 10
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 
-install_requires = parse_requirements('requirements.txt', session=PipSession())
+install_requires = parse_requirements('requirements.txt',session=PipSession())
 dependencies = [str(package.req) for package in install_requires]
 
 setup(name='hydra-python-agent',
