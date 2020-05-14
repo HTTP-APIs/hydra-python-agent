8d7d1bc25e9dc3fe0cde2b058fb8ef164e1f7499
priyanshunayan <priyanshunayan456@gmail.com>
2020-05-14 15:34:33 +0530

    change setup.py to prevent build error

--- setup.py
+++ setup.py
@@ -7,19 +7,24 @@ try:
     # pip >=20
     from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
+    install_requires = parse_requirements('requirements.txt',session=PipSession())
+    dependencies = [str(package.requirement) for package in install_requires]
 except ImportError:
     try:
         # 10.0.0 <= pip <= 19.3.1
         from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
+        from pip._internal.req import parse_requirements       
     except ImportError:
         # pip <= 9.0.3
         from pip.download import PipSession
         from pip.req import parse_requirements
 
+    install_requires = parse_requirements('requirements.txt',session=PipSession())
+    dependencies = [str(package.req) for package in install_requires]
 
-install_requires = parse_requirements('requirements.txt',session=PipSession())
-dependencies = [str(package.req) for package in install_requires]
+for package_index in range(len(dependencies)):
+  if dependencies[package_index].startswith('git+'):
+      dependencies[package_index] = dependencies[package_index].split('=')[1]
 
 setup(name='hydra-python-agent',
       include_package_data=True,
